273fbce6fc7f0b9e24795c21520a29383da4e423
criston <criston.mascarenhas@globallogic.com>
2021-05-29 19:45:41 +0530

    0a3dd6c

---
+++ _nuxt/f1a1688.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{288:function(n,t,e){"use strict";e.r(t);var r=e(10),component=Object(r.a)({},(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",{staticClass:"container"},[n._v("\n\tError in production\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ _nuxt/static/1622297688/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622297688",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d735ad70"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
