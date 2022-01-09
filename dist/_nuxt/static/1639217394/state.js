2039e01f2f1da244707412560e798d4a6e0bb28f
Criston <criston2011@gmail.com>
2022-01-09 23:28:49 +0530

    add new proj and updated resume link

--- dist/_nuxt/static/1639217394/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639217394",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"26dd7426"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
