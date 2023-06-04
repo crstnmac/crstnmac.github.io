d2a2e17acc24d2340adecc47cdd6112267714afb
crstnmac <criston2011@gmail.com>
2023-06-04 13:46:16 +0530

    Deploy

--- out/_next/static/chunks/app/(pages)/about/page-db1f6e121211a0f5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[588,281,962,346],{},function(n){n.O(0,[611,24,526,878,667,488,744],function(){return n(n.s=4878)}),_N_E=n.O()}]);
---
+++ out/_next/static/chunks/app/(pages)/layout-6e4d9c92fa9c218b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[962,281,346,588],{},function(n){n.O(0,[611,24,526,354,667,488,744],function(){return n(n.s=9354)}),_N_E=n.O()}]);
