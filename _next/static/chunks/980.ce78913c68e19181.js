939bb8817496a5960acbd9ba642e8112a98c9e1f
crstnmac <criston2011@gmail.com>
2023-06-04 13:45:12 +0530

    Deploy

---
+++ _next/static/chunks/980.ce78913c68e19181.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[980],{7980:function(t,e,n){n.r(e),n.d(e,{default:function(){return c}});var u=n(9268),r=n(8709);function c(t){let{children:e}=t;return(0,u.jsx)(r.f,{attribute:"class",children:e})}}}]);
