a5a50aeb8de355760873eeff4627830f1d78a3b0
crstnmac <criston2011@gmail.com>
2023-06-04 14:26:59 +0530

    Deploy

---
+++ out/_next/static/chunks/app/(pages)/about/page-7801338cf6d74f39.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[588,281,962,346],{},function(n){n.O(0,[70,839,624,897,667,488,744],function(){return n(n.s=897)}),_N_E=n.O()}]);
---
+++ out/_next/static/chunks/app/(pages)/page-e7cb38f19ecd1366.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[281,962,588,346],{},function(n){n.O(0,[70,839,624,897,667,488,744],function(){return n(n.s=897)}),_N_E=n.O()}]);
