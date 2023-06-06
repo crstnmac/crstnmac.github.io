f0511d2d680f601e372dc9110fbfff3b645e1034
crstnmac <criston2011@gmail.com>
2023-06-06 10:23:25 +0530

    refactor(app): :wrench: cleanup

--- next.config.js
+++ next.config.js
@@ -11,19 +11,6 @@ module.exports = withContentlayer({
   images: {
     unoptimized: true,
   },
-  // images: {
-  //   dangerouslyAllowSVG: true,
-  //   domains: [
-  //     "user-images.githubusercontent.com",
-  //     "avatars.githubusercontent.com",
-  //     "raw.githubusercontent.com",
-  //     "lanyard.cnrad.dev",
-  //     "discord.com",
-  //     "grainy-gradients.vercel.app",
-  //     "img.freepik.com",
-  //   ],
-  //   contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  // },
   reactStrictMode: true,
   webpack(config) {
     config.infrastructureLogging = {
