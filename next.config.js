64a1449f4ef67a897d70ca3032f78119c5624034
crstnmac <criston2011@gmail.com>
2023-06-06 09:56:36 +0530

    fix(app): :ambulance: images

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+
+const {withContentlayer} = require("next-contentlayer");
+
+module.exports = withContentlayer({
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "https://crstnmac.github.io": undefined,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  // images: {
+  //   dangerouslyAllowSVG: true,
+  //   domains: [
+  //     "user-images.githubusercontent.com",
+  //     "avatars.githubusercontent.com",
+  //     "raw.githubusercontent.com",
+  //     "lanyard.cnrad.dev",
+  //     "discord.com",
+  //     "grainy-gradients.vercel.app",
+  //     "img.freepik.com",
+  //   ],
+  //   contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  // },
+  reactStrictMode: true,
+  webpack(config) {
+    config.infrastructureLogging = {
+      level: "error",
+    };
+    return config;
+  },
+})
