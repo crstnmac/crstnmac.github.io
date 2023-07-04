75dd73f2b3479b404d142eaf6af5411c5297be04
crstnmac <criston2011@gmail.com>
2023-07-04 09:52:46 +0530

    fix(components): :bug: fix provider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,37 @@
 'use client'
 import Background from 'components/background'
-import { t } from 'lib'
-import dynamic from 'next/dynamic'
-import { Inter, Fira_Code } from 'next/font/google'
+// import {t} from 'lib'
+// import dynamic from 'next/dynamic'
+import {Inter, Fira_Code} from 'next/font/google'
 import React from 'react'
 import './globals.css'
-const Providers = dynamic(() => import('components/providers'), { ssr: false })
+// const Providers = dynamic(() => import('components/providers'), { ssr: false })
 
-import { useTheme } from 'next-themes'
+import {useTheme} from 'next-themes'
+import {Providers} from 'components'
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-inter'
+  variable: '--font-inter',
 })
 
 const firaCode = Fira_Code({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-fira-code'
+  variable: '--font-fira-code',
 })
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  const { resolvedTheme } = useTheme()
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  const {resolvedTheme} = useTheme()
 
   return (
     <html lang="en" className={`${inter.variable} ${firaCode.variable}`}>
       <head />
       <body
-        className={'group antialiased min-h-screen dark:bg-black-900 bg-white-300 transition-colors duration-1000 ease-in-out'}
+        className={
+          'group antialiased min-h-screen dark:bg-black-900 bg-white-300 transition-colors duration-1000 ease-in-out'
+        }
       >
         <Providers>
           <Background
