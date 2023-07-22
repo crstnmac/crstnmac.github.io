366adcae8671818d38a661bb10c76e574414b676
crstnmac <criston2011@gmail.com>
2023-07-22 13:21:27 +0530

    feat(app): :sparkles: add privacy policy page

---
+++ app/(pages)/privacy/privacy.tsx
@@ -0,0 +1,11 @@
+import {PageMeta} from 'components'
+
+export default function InfoHead() {
+  return (
+    <PageMeta
+      title="Privacy Policy"
+      description="Details about the privacy policy"
+      slug="/privacy"
+    />
+  )
+}
