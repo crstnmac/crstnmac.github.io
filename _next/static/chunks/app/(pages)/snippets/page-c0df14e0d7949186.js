e6fc82fa58a650a9419185a5d8c6e0b2ab17d87e
Criston Mascarenhas <criston2011@gmail.com>
2023-11-08 09:56:13 +0530

    Deploy

--- _next/static/chunks/app/(pages)/snippets/page-c0df14e0d7949186.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[346,281,457,588],{},function(n){n.O(0,[976,969,563,989,658,744],function(){return n(n.s=5563)}),_N_E=n.O()}]);
--- _next/static/ujy1h4YXs0-QrO2qD3xl4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-23e1077253e07b0e.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
